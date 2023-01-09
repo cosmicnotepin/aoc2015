9b238730f8eb9ddc123c2304a9c9cc5ae4252a00
dmad <dmad@t14g3.fritz.box>
2023-01-09 20:40:31 +0100

    day 9 python most easy

--- 9/p.py
+++ 9/p.py
@@ -1,17 +1,54 @@
 from timeit import default_timer as timer
+from collections import defaultdict
 
 def  main1(filename):
+    dists = defaultdict(dict)
     with open(filename, "r") as f:
-        line = f.read()
+        for line in f:
+            fr, _, to, _, d  = line.split()
+            dists[fr][to] = int(d)
+            dists[to][fr] = int(d)
 
-    return ""
+    for dest in [*(dists.keys())]:
+        dists["start"][dest] = 0
+
+    def dfs(pos, visited):
+        mn = 9999999
+        for dest in dists["start"].keys():
+            if dest in visited:
+                continue
+            mn = min(mn, dfs(dest, visited | set([dest])) + dists[pos][dest])
+        if mn == 9999999:
+            return 0
+        return mn
+
+
+    visited = set()
+    return str(dfs("start", visited))
 
 
 def  main2(filename):
+    dists = defaultdict(dict)
     with open(filename, "r") as f:
-        line = f.read()
+        for line in f:
+            fr, _, to, _, d  = line.split()
+            dists[fr][to] = int(d)
+            dists[to][fr] = int(d)
 
-    return ""
+    for dest in [*(dists.keys())]:
+        dists["start"][dest] = 0
+
+    def dfs(pos, visited):
+        mn = 0
+        for dest in dists["start"].keys():
+            if dest in visited:
+                continue
+            mn = max(mn, dfs(dest, visited | set([dest])) + dists[pos][dest])
+        return mn
+
+
+    visited = set()
+    return str(dfs("start", visited))
 
 
 if __name__ == "__main__":
