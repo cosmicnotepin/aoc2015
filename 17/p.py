25ef6c17f1b38fbc726e5da15d28929a7f290a93
dmad <dmad@t14g3.fritz.box>
2023-01-15 23:18:20 +0100

    dynamic programming? from reddit

--- 17/p.py
+++ 17/p.py
@@ -1,12 +1,30 @@
 from timeit import default_timer as timer
 from itertools import combinations, chain
 
+def count_combinations(container_sizes, target_sum):
+    dp = [1] + [0]*(target_sum)
+    for cur_num in container_sizes:
+        for next_num in range(target_sum, cur_num-1, -1):
+            dp[next_num] += dp[next_num - cur_num]
+    return dp[target_sum]
+
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 def main1(filename):
+    with open(filename, "r") as f:
+        cntnr = [int(n) for n in f.read().split()]
+        
+    eggnog = 150
+    if len(cntnr) == 5:
+        eggnog = 25
+
+    #return str(sum(map(lambda l: sum(l) == eggnog, powerset(cntnr))))
+    return str(count_combinations(cntnr, eggnog))
+
+def main1_old(filename):
     with open(filename, "r") as f:
         cntnr = [int(n) for n in f.read().split()]
         
